4b00804c2913aa1b9c77154e9ac72ed92e2ef848
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-08-09 13:30:43 -0500

    Created address class

---
+++ src/main/java/santiagotettamanti/com/doodleadoptions/address/address.java
@@ -0,0 +1,8 @@
+package santiagotettamanti.com.doodleadoptions.address;
+
+public class address {
+    private String street;
+    private String city;
+    private String state;
+    private int zipCode;
+}
