f56c9aca1ad73494e484610fd480e08f03098377
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-08-09 13:31:20 -0500

    Created Shelter class

---
+++ src/main/java/santiagotettamanti/com/doodleadoptions/shelter/Shelter.java
@@ -0,0 +1,19 @@
+package santiagotettamanti.com.doodleadoptions.shelter;
+
+public class Shelter {
+    private int shelterId;
+    private String phoneNumber;
+
+    public int getShelterId() {
+        return shelterId;
+    }
+    public void setShelterId(int shelterId) {
+        this.shelterId = shelterId;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
