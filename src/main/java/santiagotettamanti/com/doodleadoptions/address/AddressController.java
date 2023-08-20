09c6911ca6e6f99df27685c5322340eec4ce114e
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-08-20 16:11:00 -0500

    Added createAddress method in controller

--- src/main/java/santiagotettamanti/com/doodleadoptions/address/AddressController.java
+++ src/main/java/santiagotettamanti/com/doodleadoptions/address/AddressController.java
@@ -1,4 +1,19 @@
 package santiagotettamanti.com.doodleadoptions.address;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public class AddressController {
+    private final AddressService addressService;
+    @Autowired
+    public AddressController(AddressService addressService){
+        this.addressService = addressService;
+    }
+
+    @PostMapping ("/address")
+    public Address createAddress(@RequestBody Address address) {
+        return null;
+    }
+
 }
