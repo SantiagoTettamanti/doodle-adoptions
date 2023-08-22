a6c98626fe1edaad6669dc27f551cfbe3314e3d3
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-08-22 18:59:59 -0500

    add missing stuff

---
+++ src/main/java/santiagotettamanti/com/doodleadoptions/DoodleAdoptionsApplication.java
@@ -0,0 +1,13 @@
+package santiagotettamanti.com.doodleadoptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoodleAdoptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoodleAdoptionsApplication.class, args);
+	}
+
+}
