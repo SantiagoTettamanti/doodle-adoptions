f00598a404fce48af31dc0eda5f36dba890daf06
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-08-23 18:39:35 -0500

    Added annotations to AddressService

--- src/main/java/santiagotettamanti/com/doodleadoptions/address/AddressService.java
+++ src/main/java/santiagotettamanti/com/doodleadoptions/address/AddressService.java
@@ -1,7 +1,12 @@
 package santiagotettamanti.com.doodleadoptions.address;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AddressService {
+    private final AddressService addressService;
+
+    @Autowired
+
 }
