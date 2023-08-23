ee5efbed9827b9e06ee30673154c425eff613465
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-08-22 19:46:48 -0500

    Added get & post methods

--- src/main/java/santiagotettamanti/com/doodleadoptions/pet/PetController.java
+++ src/main/java/santiagotettamanti/com/doodleadoptions/pet/PetController.java
@@ -2,6 +2,8 @@ package santiagotettamanti.com.doodleadoptions.pet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +19,10 @@ public class PetController {
     @GetMapping ("/pet")
     public List<Pet> getAllPets() {
         return petService.getAllPets();
+    }
+
+    @PostMapping ("/pet")
+    public Pet createPet(@RequestBody Pet pet) {
+        return petService.createPet(pet);
     }
 }
