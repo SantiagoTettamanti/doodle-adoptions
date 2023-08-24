2c9e88470c90e46d7c87932e02d35bd010b1cd71
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-08-23 20:04:34 -0500

    Updated AddressRepository

--- src/main/java/santiagotettamanti/com/doodleadoptions/address/AddressRepository.java
+++ src/main/java/santiagotettamanti/com/doodleadoptions/address/AddressRepository.java
@@ -1,4 +1,8 @@
 package santiagotettamanti.com.doodleadoptions.address;
 
-public interface AddressRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Integer> {
 }
