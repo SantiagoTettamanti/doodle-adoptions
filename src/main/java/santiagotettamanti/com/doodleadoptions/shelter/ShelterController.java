0ce91b6780f30b17b68c6b60b5bf0a51cd8ea74c
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-08-23 20:05:12 -0500

    Added get and post methods

--- src/main/java/santiagotettamanti/com/doodleadoptions/shelter/ShelterController.java
+++ src/main/java/santiagotettamanti/com/doodleadoptions/shelter/ShelterController.java
@@ -1,4 +1,31 @@
 package santiagotettamanti.com.doodleadoptions.shelter;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import santiagotettamanti.com.doodleadoptions.address.AddressController;
+import santiagotettamanti.com.doodleadoptions.address.AddressService;
+
+import java.util.List;
+
+@RestController
 public class ShelterController {
+    private final ShelterService shelterService;
+
+    @Autowired
+    public ShelterController(ShelterService shelterService) {
+        this.shelterService = shelterService;
+    }
+
+    @GetMapping ("/shelter")
+    public List<Shelter> getAllShelters() {
+        return shelterService.getAllShelters();
+    }
+
+    @PostMapping ("/shelter")
+    public Shelter createShelter(@RequestBody Shelter shelter) {
+        return shelterService.createShelter(shelter);
+    }
 }
--- src/main/java/santiagotettamanti/com/doodleadoptions/shelter/ShelterService.java
+++ src/main/java/santiagotettamanti/com/doodleadoptions/shelter/ShelterService.java
@@ -1,4 +1,24 @@
 package santiagotettamanti.com.doodleadoptions.shelter;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ShelterService {
+    private final ShelterRepository shelterRepository;
+    @Autowired
+    public ShelterService(ShelterRepository shelterRepository) {
+        this.shelterRepository = shelterRepository;
+    }
+
+    public List<Shelter> getAllShelters() {
+        return shelterRepository.findAll();
+    }
+
+    public Shelter createShelter(Shelter shelter) {
+        return shelterRepository.save(shelter);
+    }
+
 }
