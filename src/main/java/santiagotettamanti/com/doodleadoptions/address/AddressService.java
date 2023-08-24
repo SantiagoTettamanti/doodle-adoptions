30983927a162f4513016d6827e1de1eebfae90ec
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-08-23 20:04:59 -0500

    Added get and post methods to AddressService

--- src/main/java/santiagotettamanti/com/doodleadoptions/address/AddressService.java
+++ src/main/java/santiagotettamanti/com/doodleadoptions/address/AddressService.java
@@ -2,11 +2,25 @@ package santiagotettamanti.com.doodleadoptions.address;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Service
 public class AddressService {
-    private final AddressService addressService;
+    private final AddressRepository addressRepository;
 
     @Autowired
+    public AddressService(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
 
+    public List<Address> getAllAddresses() {
+        return addressRepository.findAll();
+    }
+
+    public Address createAddress(Address address) {
+        return addressRepository.save(address);
+    }
 }
