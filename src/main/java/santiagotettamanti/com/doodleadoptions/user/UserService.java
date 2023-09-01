5de8f3570517f4a4e803bbade4faea3b5e95895d
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-08-31 20:50:46 -0500

    Updated service

--- src/main/java/santiagotettamanti/com/doodleadoptions/user/UserService.java
+++ src/main/java/santiagotettamanti/com/doodleadoptions/user/UserService.java
@@ -1,4 +1,35 @@
 package santiagotettamanti.com.doodleadoptions.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import santiagotettamanti.com.doodleadoptions.shelter.ShelterRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Integer id) {
+        Optional<User> userFound = userRepository.findById(id);
+        if (userFound.isPresent()) {
+            return userFound.get();
+        } else {
+            return null;
+        }
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
 }
