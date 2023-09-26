e5179698f313c8975e8b343d400d3ae18f7f0a7f
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-09-25 20:06:46 -0500

    Created NotificationRepository

---
+++ src/main/java/santiagotettamanti/com/doodleadoptions/notification/NotificationRepository.java
@@ -0,0 +1,8 @@
+package santiagotettamanti.com.doodleadoptions.notification;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+}
