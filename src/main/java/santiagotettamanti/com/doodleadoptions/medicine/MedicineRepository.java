69345be042423250ed981e2f202ee930b3719af3
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-09-25 20:06:12 -0500

    Created MedicineRepository

---
+++ src/main/java/santiagotettamanti/com/doodleadoptions/medicine/MedicineRepository.java
@@ -0,0 +1,8 @@
+package santiagotettamanti.com.doodleadoptions.medicine;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicineRepository extends JpaRepository<Medicine, Integer> {
+}
