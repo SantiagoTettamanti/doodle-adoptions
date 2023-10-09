30610a368b5fa4956a7571e1174f5197f3f0de9e
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-10-09 09:06:41 -0500

    Updated User

--- src/main/java/santiagotettamanti/com/doodleadoptions/user/User.java
+++ src/main/java/santiagotettamanti/com/doodleadoptions/user/User.java
@@ -1,9 +1,55 @@
 package santiagotettamanti.com.doodleadoptions.user;
 
+import jakarta.persistence.*;
+
+@Table (name = "USUARIO")
+@Entity
 public class User {
-    private int userId;
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer userId;
     private String firstName;
     private String lastName;
     private String email;
     private Integer shelterId;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getShelterId() {
+        return shelterId;
+    }
+
+    public void setShelterId(int shelterId) {
+        this.shelterId = shelterId;
+    }
 }
