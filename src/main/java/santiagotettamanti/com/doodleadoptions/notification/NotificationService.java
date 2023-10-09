3c196edd5e914d387f6575d02808a09ef493a9b3
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-10-09 09:05:13 -0500

    Updated NotificationService

--- src/main/java/santiagotettamanti/com/doodleadoptions/notification/NotificationService.java
+++ src/main/java/santiagotettamanti/com/doodleadoptions/notification/NotificationService.java
@@ -3,11 +3,39 @@ package santiagotettamanti.com.doodleadoptions.notification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class NotificationService {
     private final NotificationRepository notificationRepository;
     @Autowired
     public NotificationService(NotificationRepository notificationRepository) {
         this.notificationRepository = notificationRepository;
+    }
+
+    public List<Notification> getAllNotifications() {
+        return notificationRepository.findAll();
+    }
+    public Notification getNotificationById(Integer id) {
+        Optional<Notification> notificationFound = notificationRepository.findById(id);
+        if(notificationFound.isPresent()) {
+            return notificationFound.get();
+        } else {
+            return null;
+        }
+    }
+    public Notification createNotification(Notification notification) {
+        return notificationRepository.save(notification);
+    }
+
+    public Notification deleteNotificationById(Integer id) {
+        Notification notificationFound = getNotificationById(id);
+        if (notificationFound == null) {
+            throw new RuntimeException("error");
+        }
+        notificationRepository.deleteById(id);
+        return notificationFound;
     }
 }
