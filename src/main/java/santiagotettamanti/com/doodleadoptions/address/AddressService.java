a2121a1aef21c5aff2f086ec51e681a73c04e6b8
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-10-09 09:04:36 -0500

    Updated AddressService

--- src/main/java/santiagotettamanti/com/doodleadoptions/address/AddressService.java
+++ src/main/java/santiagotettamanti/com/doodleadoptions/address/AddressService.java
@@ -2,6 +2,7 @@ package santiagotettamanti.com.doodleadoptions.address;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import santiagotettamanti.com.doodleadoptions.pet.Pet;
 
 import java.util.List;
 import java.util.Optional;
@@ -32,4 +33,12 @@ public class AddressService {
         return addressRepository.save(address);
     }
 
+    public Address deleteAddressById(Integer id) {
+       Address addressFound = getAddressById(id);
+       if(addressFound == null) {
+           throw new RuntimeException("error");
+       }
+       addressRepository.deleteById(id);
+       return addressFound;
+    }
 }
